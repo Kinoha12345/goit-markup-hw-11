1a83654531cded693084dcedca467a637b3c217b
Kinoha12345 <filin.vlad.v@gmail.com>
2022-01-18 18:25:17 +0200

    122321312312

--- src/index.js
+++ src/index.js
@@ -6,41 +6,68 @@ const refs = {
     form: document.querySelector('#search-form'),
     input: document.querySelector('input'),
     btn: document.querySelector('.btn-sub'),
-    galleryDiv: document.querySelector('.gallery')
+    galleryDiv: document.querySelector('.gallery'),
+    btnLoadMore: document.querySelector('.load-more')
 }
-refs.form.addEventListener('submit', createGellary)
+
+let page = 1;
+let iterator = 0;
+let inputValue = '';
+
+refs.form.addEventListener('submit', createGellary);
+refs.btnLoadMore.addEventListener('click',onClickLoadMore)
+
+function createMarkup({data:{hits}}){
+   
+  return hits.map(item => `<div class="photo-card">
+  <img src="${item.webformatURL}" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <br>${item.likes}</br>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      <br>${item.views}</br>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      <br>${item.comments}</br>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      <br>${item.downloads}</br>
+    </p>
+  </div>
+</div>`).join("")
+}
 
 function createGellary(e){
-e.preventDefault()
-const inputValue = refs.input.value;
-console.log(refs.input.value)
-getDataServer(inputValue).then(data=>{
+  e.preventDefault()
+  refs.galleryDiv.innerHTML = '';
+  inputValue = refs.form.elements.searchQuery.value;
+  refs.btnLoadMore.style.display = 'inline-block';
+    getDataServer(inputValue).then(data=>{
+    Notiflix.Notify.success(`Hooray! We found ${data.data.totalHits} images.`)
+      if(data.data.hits.length === 0){
+        refs.btnLoadMore.style.display = 'none'
+        return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+    }
+    iterator = 40;
  refs.galleryDiv.insertAdjacentHTML('afterbegin',createMarkup(data));
 }).catch(console.log);
 
 }
 
-function createMarkup(data){
-   
-    return data.data.hits.map(item => `<div class="photo-card">
-    <img src="${item.webformatURL}" alt="" loading="lazy" />
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-        ${item.likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b>
-        ${item.views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b>
-        ${item.comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>
-        ${item.downloads}
-      </p>
-    </div>
-  </div>`).join("")
-}
+function onClickLoadMore () {
+  page+= 1;
+  iterator+=40;
+  getDataServer(inputValue, page).then(data =>{
+    createMarkup(data)
+    refs.galleryDiv.insertAdjacentHTML('beforeend',createMarkup(data));
+    if(data.data.totalHits <= iterator){
+      refs.btnLoadMore.style.display = 'none'
+      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
+    }
+  });
+} 
